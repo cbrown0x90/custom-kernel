86e632b540fd736266e9fd64b4750f6587fe829b
Christopher Brown <cebrown5597@gmail.com>
2017-11-11 14:56:50 -0500

    Computer bump

--- compile-kernel.sh
+++ compile-kernel.sh
@@ -7,12 +7,15 @@ cp -v /home/chris/Documents/custom-kernel/linux-ulm.config .config
 cp /bin/busybox /usr/src/initramfs/bin
 cp /sbin/cryptsetup /usr/src/initramfs/bin
 
-make -j4
+make -j8
 make modules_install
 KERNUM=$(eselect kernel list | awk -v ver=$VERSION '$2 ~ ver {print substr($1, index($1, "[") + 1, index($1, "]") - 2)}')
 eselect kernel set $KERNUM
 
 cp -v .config /home/chris/Documents/custom-kernel/linux-ulm.config
+if [ "$1" != "--no-rebuild" ]; then
+    emerge -v @module-rebuild
+fi
 
 test ! -d $dest_dir && echo "insert USB"
 while test ! -d $dest_dir; do
