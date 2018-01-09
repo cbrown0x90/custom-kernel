00867279e00a0b1fb7d8c78a239eade70e76f584
Christopher Brown <cebrown5597@gmail.com>
2018-01-09 11:44:02 -0500

    No longer use propreitary nvidia drivers

--- compile-kernel.sh
+++ compile-kernel.sh
@@ -4,8 +4,8 @@ VERSION=$(pwd | awk '{print substr($0, match($0, "[0-9]"))}')
 readonly dest_dir="/boot/gentoo"
 
 cp -v /home/chris/Documents/custom-kernel/linux-ulm.config .config
-cp /bin/busybox /usr/src/initramfs/bin
-cp /sbin/cryptsetup /usr/src/initramfs/bin
+#cp /bin/busybox /usr/src/initramfs/bin
+#cp /sbin/cryptsetup /usr/src/initramfs/bin
 
 make -j8
 make modules_install
@@ -13,9 +13,6 @@ KERNUM=$(eselect kernel list | awk -v ver=$VERSION '$2 ~ ver {print substr($1, i
 eselect kernel set $KERNUM
 
 cp -v .config /home/chris/Documents/custom-kernel/linux-ulm.config
-if [ "$1" != "--no-rebuild" ]; then
-    emerge -v @module-rebuild
-fi
 
 test ! -d $dest_dir && echo "insert USB"
 while test ! -d $dest_dir; do
